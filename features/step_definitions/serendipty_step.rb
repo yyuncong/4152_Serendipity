84d2ac175eb9ccdc253ccaae5aef8db5afd481b9
Xiyuan Zhao <xz2994@columbia.edu>
2022-11-03 02:00:10 +0000

    update loginand log out test

--- features/step_definitions/serendipty_step.rb
+++ features/step_definitions/serendipty_step.rb
@@ -3,16 +3,7 @@ Given /^"([^"]*)" is logged in$/ do |email|
     puts "user id: #{@current_user.id}"
   end
   
-
-Given /^"([^"]*)" has go to the post page$/ do |name|
-@current_user = User.create(:name=>name, :email => "#{name}@columbia.edu")
-profile=Profile.create(:major=>"CS")
-profile.user=@current_user
+Given /^I created a post with name "([^"]*)" $/ do |post_name,name1|
+this_post=Post.create!(:title=>post_name,:content=>"#{post_name} content",:user_id =>1)
 end
 
-Given /^I created a post with name "([^"]*)", and "([^"]*)" applied for it$/ do |post_name,name1|
-this_post=Post.create!(:title=>post_name,:content=>"#{post_name} content",:user_id =>1,start:"01/01/2022",end:"01/02/3022", low_number: 1, high_number:100)
-this_group=Group.create!(:post_id=>this_post.id) 
-applied_user1=User.create!(:name=>name1, :email => "test_email1")  
-GroupUser.create!(:group_id=>this_group.id, :user_id=>applied_user1.id, :status=>"applied")
-end
