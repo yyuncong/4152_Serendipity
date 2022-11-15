50814c19535239fbaf4a9695039e23374fd0ea23
yzl1998 <zy2496@columbia.edu>
2022-11-15 18:30:55 -0500

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_15_012919) do
+ActiveRecord::Schema.define(version: 2022_11_15_231357) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "post_id"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2022_11_15_012919) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -25,6 +26,7 @@ ActiveRecord::Schema.define(version: 2022_11_15_012919) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "profiles", force: :cascade do |t|
